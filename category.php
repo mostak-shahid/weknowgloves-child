ab1d470d192179a69e052a7292e080900e3e9c67
Md. Mostak Shahid <mostak.shahid@gmail.com>
2021-06-16 00:01:55 +0600

    Update category.php

--- category.php
+++ category.php
@@ -26,8 +26,7 @@ $description = get_the_archive_description();
 
 	<?php while ( have_posts() ) : ?>
 		<?php the_post(); ?>
-		Kukur
-		<?php //get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) ); ?>
+		<?php echo '<div>' . get_the_title() . '</div>'; ?>
 	<?php endwhile; ?>
 
 	<?php twenty_twenty_one_the_posts_navigation(); ?>
