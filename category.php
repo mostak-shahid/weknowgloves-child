dfd1d3521cdb1a306d3f20c2aea7f0c62274ed5c
Md. Mostak Shahid <mostak.shahid@gmail.com>
2021-06-15 23:35:09 +0600

    Update category.php

--- category.php
+++ category.php
@@ -17,7 +17,8 @@ $description = get_the_archive_description();
 <?php if ( have_posts() ) : ?>
 
 	<header class="page-header alignwide">
-		<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
+		<?php //the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
+		<h2><?php single_cat_title( '',true )?></h2>
 		<?php if ( $description ) : ?>
 			<div class="archive-description"><?php echo wp_kses_post( wpautop( $description ) ); ?></div>
 		<?php endif; ?>
